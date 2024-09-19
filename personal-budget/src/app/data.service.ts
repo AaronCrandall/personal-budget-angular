c7e440bdf7608d792a7683be8af40510cb6ac07e
Aaron Crandall <117679437+AaronCrandall@users.noreply.github.com>
2024-09-19 18:05:37 -0400

    After finally receiving the smallest hint from one of the TAs I was able to do the research and figure out how to make this work, no thanks to all the TAs and Instructor just saying what the problem was without any hint of how to fix it I spent about 10 hours 'solving' the problem in incorrect ways

--- personal-budget/src/app/data.service.ts
+++ personal-budget/src/app/data.service.ts
@@ -6,7 +6,9 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
-  private data = [{"Framework": "", "Stars": ""}];
+  public Chart1Observable;
+  public Chart2Observable;
+  public data = [{"Framework": "", "Stars": ""}];
   public dataSource: any = {
     datasets: [
         {
@@ -26,25 +28,38 @@ export class DataService {
   };
   constructor(private http: HttpClient) {
     if(this.dataSource.datasets[0].data[0] === undefined){
-      this.http.get('http://localhost:3000/budget').subscribe((res: any)=>{
-        //console.log(res);
-        for (var i = 0; i < res.myBudget.length; i++) {
-          this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
-          this.dataSource.labels[i] = res.myBudget[i].title;
-        }
-        for(var i = 0; i < this.dataSource.labels.length; i++){
-          var datas = {"Framework": this.dataSource.labels[i], "Stars":this.dataSource.datasets[0].data[i]}
-          this.data.push(datas);
-        }
+      this.Chart1Observable = new Observable((observer)=>{
+        this.http.get('http://localhost:3000/budget').subscribe((res: any)=>{
+          //console.log(res);
+          for (var i = 0; i < res.myBudget.length; i++) {
+            this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
+            this.dataSource.labels[i] = res.myBudget[i].title;
+          }
+          for(var i = 0; i < this.dataSource.labels.length; i++){
+            var datas = {"Framework": this.dataSource.labels[i], "Stars":this.dataSource.datasets[0].data[i]}
+            this.data.push(datas);
+          }
+          observer.next(this.dataSource);
+        });
+      });
+      this.Chart2Observable = new Observable((observer)=>{
+        if(this.dataSource.datasets[0].data[0] === undefined){
+        this.http.get('http://localhost:3000/budget').subscribe((res: any)=>{
+          observer.next(this.data);
+        });
+      }
+      //observer.next(this.data);
+      });
+    }
+    else{
+      this.Chart1Observable = new Observable((observer) => {
+        observer.next(this.dataSource);
+      });
+      this.Chart2Observable = new Observable((observer) => {
+        observer.next(this.data);
       });
     }
   }
-  public getAllData(){
-    console.log(this.dataSource);
-    return this.dataSource;
-  }
-  public getD3jsData(){
-    console.log(this.data);
-    return this.data;
-  }
+  //public getAllData(){}
+  //public getD3jsData(){}
 }
--- personal-budget/src/app/homepage/homepage.component.ts
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -5,6 +5,7 @@ import { Chart } from 'chart.js/auto';
 import { BreadcrumbsComponent } from '../breadcrumbs/breadcrumbs.component';
 import * as d3 from 'd3';
 import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -15,6 +16,7 @@ import { DataService } from '../data.service';
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
+  Chart2Observable = new Observable();
   dataservice: DataService = inject(DataService);
   private svg: any;
   private margin = 50;
@@ -23,47 +25,35 @@ export class HomepageComponent {
   // The radius of the pie chart is half the smallest side
   private radius = Math.min(this.width, this.height) / 2 - this.margin;
   private colors: any;
-  private data1 = [{Framework: '', Stars: ''},{Framework: 'Eat out', Stars: 25},
-  {Framework: 'Rent', Stars: 275},{Framework: 'Grocery', Stars: 110},{Framework: 'Insurance', Stars: 50},
-  {Framework: 'Phone Bill', Stars: 75},{Framework: 'Savings', Stars: 250},{Framework: 'Internet', Stars: 45}]
-  //private data = this.dataservice.getD3jsData();
-  public dataSource: any = {
-    datasets: [
-        {
-            data: [25, 275, 110, 50, 75, 250, 45],
-            backgroundColor: [
-                '#ffcd56',
-                '#ff6384',
-                '#36a2eb',
-                '#fd6b19',
-                '#abc4ff',
-                '#b8e0d2',
-                '#e8dff5',
-            ]
-        }
-    ],
-    labels: ["Eat out","Rent","Grocery","Insurance","Phone Bill","Savings","Internet"]
-  };
+  //private data1 = [{Framework: '', Stars: ''},{Framework: 'Eat out', Stars: 25},
+  //{Framework: 'Rent', Stars: 275},{Framework: 'Grocery', Stars: 110},{Framework: 'Insurance', Stars: 50},
+  //{Framework: 'Phone Bill', Stars: 75},{Framework: 'Savings', Stars: 250},{Framework: 'Internet', Stars: 45}]
+  private data: any;
   //private dataSource = this.dataservice.getAllData();
   constructor(){
 
   }
   ngOnInit(): void{
-      console.log(this.dataSource);
-      this.createChart();
+    this.dataservice.Chart1Observable!.subscribe((val) => {
+      this.createChart(val);
+    })
+    this.dataservice.Chart2Observable!.subscribe((val) => {
       this.createSvg();
-      this.createColors();
-      this.createChart2();
+      this.createColors(val);
+      this.createChart2(val);
+    })
+      //this.createChart();
+      //this.createSvg();
+      //this.createColors();
+      //this.createChart2();
   }
 
-  createChart() {
+  createChart(val: any) {
     var canvas = <HTMLCanvasElement> document.getElementById('myChart')!;
-    var NewData = this.dataservice.getAllData();
-    console.log(NewData);
     var ctx = canvas.getContext('2d')!;
     var myPieChart = new Chart(ctx, {
         type: 'pie',
-        data: this.dataSource
+        data: val
     });
   }
   createSvg(): void {
@@ -77,19 +67,19 @@ export class HomepageComponent {
       "translate(" + this.width / 2 + "," + this.height / 2 + ")"
     );
   }
-  createColors(): void {
+  createColors(val: any): void {
     this.colors = d3.scaleOrdinal()
-    .domain(this.data1.map((d: any) => d.Stars.toString()))
+    .domain(val.map((d: any) => d.Stars.toString()))
     .range(['#ffcd56','#ff6384','#36a2eb','#fd6b19','#abc4ff','#b8e0d2','#e8dff5']);
   }
-  createChart2(): void {
+  createChart2(val: any): void {
     // Compute the position of each group on the pie:
   const pie = d3.pie<any>().value((d: any) => Number(d.Stars));
     //console.log(this.data);
   // Build the pie chart
   this.svg
   .selectAll('pieces')
-  .data(pie(this.data1))
+  .data(pie(val))
   .enter()
   .append('path')
   .attr('d', d3.arc()
@@ -107,10 +97,10 @@ export class HomepageComponent {
 
   this.svg
   .selectAll('pieces')
-  .data(pie(this.data1))
+  .data(pie(val))
   .enter()
   .append('text')
-  .text((d: any)=> d.data1.Framework)
+  .text((d: any)=> d.val.Framework)
   .attr("transform", (d: any) => "translate(" + labelLocation.centroid(d) + ")")
   .style("text-anchor", "middle")
   .style("font-size", 15);
